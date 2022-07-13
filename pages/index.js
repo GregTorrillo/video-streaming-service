f0d9ea2bc8f2477bad058a87529216ce52530d53
Greg Torrillo <greg.torrillo@gmail.com>
2022-07-12 23:03:17 -0400

    Created Lazy Load components

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import MainLayout from "../components/Layouts/MainLayout";
 import FeaturedMedia from "../components/UI/FeaturedMedia/FeaturedMedia";
 import MediaRow from "../components/UI/MediaRow/MediaRow";
 import AuthCheck from "../components/AuthCheck";
+import LazyLoad from 'react-lazyload';
 
 export default function Home() {
   const globalState = useStateContext();
@@ -14,36 +15,48 @@ export default function Home() {
   return AuthCheck(
     <MainLayout>
       <FeaturedMedia />
+      <LazyLoad height={680} offset={-400} placeholder={<h1>Loading</h1>}>
       <MediaRow
         title="Movies"
         type="large-v"
         endpoint="discover/movie?sort_by=popularity.desc&primary_release_year=2021"
       />
+      </LazyLoad>
+      <LazyLoad height={333} offset={-400}>
       <MediaRow
         title="Series"
         type="small-h"
         endpoint="discover/tv?primary_release_year=2021"
       />
+      </LazyLoad>
+      <LazyLoad height={440} offset={-400}>
       <MediaRow
         title="Action"
         type="small-v"
         endpoint="discover/movie?with_genres=28&primary_release_year=2021"
       />
+      </LazyLoad>
+      <LazyLoad height={200}>
       <MediaRow
         title="Horror"
         type="small-v"
         endpoint="discover/movie?with_genres=27&primary_release_year=2021"
       />
+      </LazyLoad>
+      <LazyLoad height={200}>
       <MediaRow
         title="Animation"
         type="large-h"
         endpoint="discover/movie?with_genres=16&primary_release_year=2021"
       />
+      </LazyLoad>
+      <LazyLoad height={200}>
       <MediaRow
         title="Sci-fi"
         type="small-v"
         endpoint="discover/movie?with_genres=878&primary_release_year=2021"
       />
+      </LazyLoad>
     </MainLayout>
   );
 }
