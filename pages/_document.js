80967a703f0a6b2b739aec27d0887e5da9cf7297
Greg Torrillo <greg.torrillo@gmail.com>
2022-07-14 16:28:18 -0400

    Added local fontawesome

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         {/* <link rel="preconnect" href="https://fonts.googleapis.com"/> */}
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
         <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;300;400;500;700&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
-        <link rel="preload" as="style" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" integrity="sha384-/frq1SRXYH/bSyou/HUp/hib7RVN1TawQYja658FEOodR/FQBKVqT9Ol+Oz3Olq5" crossOrigin="anonymous"/>
+        <link rel="stylesheet" href="/fontaw/css/all.min.css" />
       </Head>
       <body>
         <Main />
