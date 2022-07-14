6a40639b144be9012b0176f2e4c45f70a737c380
Greg Torrillo <greg.torrillo@gmail.com>
2022-07-14 16:14:59 -0400

    Created Placeholders and Optimized

--- pages/_document.js
+++ pages/_document.js
@@ -4,10 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com"/> */}
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;300;400;500;700&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" integrity="sha384-/frq1SRXYH/bSyou/HUp/hib7RVN1TawQYja658FEOodR/FQBKVqT9Ol+Oz3Olq5" crossOrigin="anonymous"/>
+        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@100;300;400;500;700&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
+        <link rel="preload" as="style" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css" integrity="sha384-/frq1SRXYH/bSyou/HUp/hib7RVN1TawQYja658FEOodR/FQBKVqT9Ol+Oz3Olq5" crossOrigin="anonymous"/>
       </Head>
       <body>
         <Main />
