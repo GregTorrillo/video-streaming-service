a36bccd204770dcc533e2fc75e88b066d966ae43
Greg Torrillo <greg.torrillo@gmail.com>
2022-07-14 18:09:06 -0400

    Created an individual Movie Page

--- pages/movie.js
+++ pages/movie.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
+import AuthCheck from '../components/AuthCheck'
 import MainLayout from '../components/Layouts/MainLayout'
 import CastInfo from '../components/UI/CastInfo/CastInfo'
 import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia'
 import MediaRow from '../components/UI/MediaRow/MediaRow'
 
 export default function HomeView() {
-  return (
+  return AuthCheck(
     <MainLayout>
       <FeaturedMedia />
       <MediaRow title="More Like This" type="small-v" />
---
+++ pages/movie/[id].js
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import {useEffect, useState} from 'react'
+import AuthCheck from '../../components/AuthCheck'
+import MainLayout from '../../components/Layouts/MainLayout'
+import CastInfo from '../../components/UI/CastInfo/CastInfo'
+import FeaturedMedia from '../../components/UI/FeaturedMedia/FeaturedMedia'
+import MediaRow from '../../components/UI/MediaRow/MediaRow'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+
+export default function SingleMediaPage(props) {
+    const router = useRouter()
+    const [mediaData, setMediaData] = useState(false);
+    // const { id } = router.query 
+    console.log(props)
+
+    useEffect(() => {
+        axios
+            .get(`https://api.themoviedb.org/3/movie/${props.query.id}?api_key=71bbad38cd74cecf8e7ec1d5e478a67a&language=en-US`)
+            .then(function (response) {
+                setMediaData(response.data)
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log('Error Response For ')
+                console.log(error);
+            })
+    }, [])
+
+  return AuthCheck(
+    <MainLayout>
+        <h1 style={{color: 'white'}}>${props.query.id}</h1>
+      <FeaturedMedia title={mediaData.title} />
+      <MediaRow title="More Like This" type="small-v" />
+      <CastInfo />
+    </MainLayout>  
+  )
+}
+
+export async function getServerSideProps(context){
+    return {
+        props: {query: context.query},// will be passed to the page component as props 
+    }
+}
