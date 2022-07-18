fbaac88730cdaec462a6fcf3cbd1cf4fdae1c794
Greg Torrillo <greg.torrillo@gmail.com>
2022-07-18 14:36:23 -0400

    Fixed Side Navigation

--- components/UI/SideNav/SideNav.js
+++ components/UI/SideNav/SideNav.js
@@ -1,97 +1,39 @@
-// import Link from 'next/link'
-import { useStateContext } from '../../Provider'
+import Link from "next/link";
+import { useStateContext } from "../../Provider";
 
 const SideNav = (props) => {
-    const globalState = useStateContext();
-    return (
-    <div className={`side-nav ${globalState.sideNavOpen ? 
-    'side-nav--active' : ''}`}>
-        <div className="side-nav__close-btn" onClick={() => globalState.setSideNavOpenAction(false)}>
-            <i className="fas fa-times" />
-        </div>
-        <ul className="side-nav__main">
-            <li>
-                <a href="/" className="active">
-                    Home
-                </a>
-            </li>
-            <li>
-                <a href="/">Series</a>
-            </li>
-            <li>
-                <a href="/">Movies</a>
-            </li>
-            <li>
-                <a href="/">Originals</a>
-            </li>
-            <li>
-                <a href="/">Just Added</a>
-            </li>
-            <li>
-                <a href="/">Last Chance</a>
-            </li>
-            <li>
-                <a href="/">Coming Soon</a>
-            </li>
-            <li>
-                <a href="/">Trending Now</a>
-            </li>
-        </ul>
-        <div className="side-nav__divider" />
-        <ul className="side-nav__main">
-            <li>
-                <a href="/">Action</a>
-            </li>
-            <li>
-                <a href="/">Animation</a>
-            </li>
-            <li>
-                <a href="/">Comedy</a>
-            </li>
-            <li>
-                <a href="/">Crime</a>
-            </li>
-            <li>
-                <a href="/">Documentaries</a>
-            </li>
-            <li>
-                <a href="/">Drama</a>
-            </li>
-            <li>
-                <a href="/">Fantasy & Sci-fi</a>
-            </li>
-            <li>
-                <a href="/">Horror</a>
-            </li>
-            <li>
-                <a href="/">International</a>
-            </li>
-            <li>
-                <a href="/">Kids & Family</a>
-            </li>
-            <li>
-                <a href="/">Latino</a>
-            </li>
-            <li>
-                <a href="/">Music</a>
-            </li>
-            <li>
-                <a href="/">News/Talk</a>
-            </li>
-            <li>
-                <a href="/">Reality</a>
-            </li>
-            <li>
-                <a href="/">Romance</a>
-            </li>
-            <li>
-                <a href="/">Shorts</a>
-            </li>
-            <li>
-                <a href="/">Suspense</a>
-            </li>
-        </ul>
-    </div>)
-}
+  const globalState = useStateContext();
+  return (
+    <div
+      className={`side-nav ${
+        globalState.sideNavOpen ? "side-nav--active" : ""
+      }`}
+    >
+      <div
+        className="side-nav__close-btn"
+        onClick={() => globalState.setSideNavOpenAction(false)}
+      >
+        <i className="fas fa-times" />
+      </div>
+      <ul className="side-nav__main">
+        <li onClick={() => globalState.setSideNavOpenAction(false)}>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li onClick={() => globalState.setSideNavOpenAction(false)}>
+          <Link href="/movie">
+            <a>Movies</a>
+          </Link>
+        </li>
+        <li onClick={() => globalState.setSideNavOpenAction(false)}>
+          <Link href="/tv">
+            <a>Series</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default SideNav; 
+export default SideNav;
